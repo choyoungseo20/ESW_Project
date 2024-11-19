e18ee4a508a913fa6dd7cfbf701e21bedfa7f82b
choyoungseo <whdudtj0809@naver.com>
2024-11-19 09:31:52 +0900

    Term UI 완료

---
+++ py_code/Term.py
@@ -0,0 +1,224 @@
+import numpy as np
+
+class Term1_1:
+    def __init__(self):
+        self.position = None
+        self.size = None
+        self.pixel_map = [
+            "..d.........d..",
+            ".dd........dd..",
+            "..d.........d..",
+            "..d...ddd...d..",
+            "..d.........d..",
+            "..d.........d..",
+            ".ddd.......ddd.",
+            ]
+    
+    def setSize(self, x, y, size):
+        self.position = np.array([x, y])
+        self.size = size
+
+    def draw(self, draw_tool):
+        x_start, y_start = self.position
+        for y, row in enumerate(self.pixel_map):
+            for x, pixel in enumerate(row):
+                if pixel == "d":
+                    x0 = x_start + x * self.size
+                    y0 = y_start + y * self.size
+                    draw_tool.rectangle([x0, y0, x0 + self.size, y0 + self.size], fill = (0, 0, 0))
+
+
+class Term1_2:
+    def __init__(self):
+        self.position = None
+        self.size = None
+        self.pixel_map = [
+            "..d........ddd.",
+            ".dd.......d...d",
+            "..d...........d",
+            "..d...ddd....d.",
+            "..d.........d..",
+            "..d........d...",
+            ".ddd......ddddd",
+            ]
+
+    def setSize(self, x, y, size):
+        self.position = np.array([x, y])
+        self.size = size
+
+    def draw(self, draw_tool):
+        x_start, y_start = self.position
+        for y, row in enumerate(self.pixel_map):
+            for x, pixel in enumerate(row):
+                if pixel == "d":
+                    x0 = x_start + x * self.size
+                    y0 = y_start + y * self.size
+                    draw_tool.rectangle([x0, y0, x0 + self.size, y0 + self.size], fill = (0, 0, 0))
+
+
+class Term2_1:
+    def __init__(self):
+        self.position = None
+        self.size = None
+        self.pixel_map = [
+            ".ddd........d..",
+            "d...d......dd..",
+            "....d.......d..",
+            "...d..ddd...d..",
+            "..d.........d..",
+            ".d..........d..",
+            "ddddd......ddd.",
+            ]
+
+    def setSize(self, x, y, size):
+        self.position = np.array([x, y])
+        self.size = size
+
+    def draw(self, draw_tool):
+        x_start, y_start = self.position
+        for y, row in enumerate(self.pixel_map):
+            for x, pixel in enumerate(row):
+                if pixel == "d":
+                    x0 = x_start + x * self.size
+                    y0 = y_start + y * self.size
+                    draw_tool.rectangle([x0, y0, x0 + self.size, y0 + self.size], fill = (0, 0, 0))
+
+
+class Term2_2:
+    def __init__(self):
+        self.position = None
+        self.size = None
+        self.pixel_map = [
+            ".ddd.......ddd.",
+            "d...d.....d...d",
+            "....d.........d",
+            "...d..ddd....d.",
+            "..d.........d..",
+            ".d.........d...",
+            "ddddd.....ddddd",
+            ]
+
+    def setSize(self, x, y, size):
+        self.position = np.array([x, y])
+        self.size = size
+
+    def draw(self, draw_tool):
+        x_start, y_start = self.position
+        for y, row in enumerate(self.pixel_map):
+            for x, pixel in enumerate(row):
+                if pixel == "d":
+                    x0 = x_start + x * self.size
+                    y0 = y_start + y * self.size
+                    draw_tool.rectangle([x0, y0, x0 + self.size, y0 + self.size], fill = (0, 0, 0))
+
+
+class Term3_1:
+    def __init__(self):
+        self.position = None
+        self.size = None
+        self.pixel_map = [
+            ".ddd........d..",
+            "d...d......dd..",
+            "....d.......d..",
+            "..dd..ddd...d..",
+            "....d.......d..",
+            "d...d.......d..",
+            ".ddd.......ddd.",
+            ]
+
+    def setSize(self, x, y, size):
+        self.position = np.array([x, y])
+        self.size = size
+
+    def draw(self, draw_tool):
+        x_start, y_start = self.position
+        for y, row in enumerate(self.pixel_map):
+            for x, pixel in enumerate(row):
+                if pixel == "d":
+                    x0 = x_start + x * self.size
+                    y0 = y_start + y * self.size
+                    draw_tool.rectangle([x0, y0, x0 + self.size, y0 + self.size], fill = (0, 0, 0))
+
+
+class Term3_2:
+    def __init__(self):
+        self.position = None
+        self.size = None
+        self.pixel_map = [
+            ".ddd.......ddd.",
+            "d...d.....d...d",
+            "....d.........d",
+            "..dd..ddd....d.",
+            "....d.......d..",
+            "d...d......d...",
+            ".ddd......ddddd",
+            ]
+
+    def setSize(self, x, y, size):
+        self.position = np.array([x, y])
+        self.size = size
+
+    def draw(self, draw_tool):
+        x_start, y_start = self.position
+        for y, row in enumerate(self.pixel_map):
+            for x, pixel in enumerate(row):
+                if pixel == "d":
+                    x0 = x_start + x * self.size
+                    y0 = y_start + y * self.size
+                    draw_tool.rectangle([x0, y0, x0 + self.size, y0 + self.size], fill = (0, 0, 0))
+
+
+class Term4_1:
+    def __init__(self):
+        self.position = None
+        self.size = None
+        self.pixel_map = [
+            "...d........d..",
+            "..dd.......dd..",
+            ".d.d........d..",
+            "d..d..ddd...d..",
+            "ddddd.......d..",
+            "...d........d..",
+            "...d.......ddd.",
+            ]
+
+    def setSize(self, x, y, size):
+        self.position = np.array([x, y])
+        self.size = size
+
+    def draw(self, draw_tool):
+        x_start, y_start = self.position
+        for y, row in enumerate(self.pixel_map):
+            for x, pixel in enumerate(row):
+                if pixel == "d":
+                    x0 = x_start + x * self.size
+                    y0 = y_start + y * self.size
+                    draw_tool.rectangle([x0, y0, x0 + self.size, y0 + self.size], fill = (0, 0, 0))
+
+
+class Term4_2:
+    def __init__(self):
+        self.position = None
+        self.size = None
+        self.pixel_map = [
+            "...d.......ddd.",
+            "..dd......d...d",
+            ".d.d..........d",
+            "d..d..ddd....d.",
+            "ddddd.......d..",
+            "...d.......d...",
+            "...d......ddddd",
+            ]
+
+    def setSize(self, x, y, size):
+        self.position = np.array([x, y])
+        self.size = size
+
+    def draw(self, draw_tool):
+        x_start, y_start = self.position
+        for y, row in enumerate(self.pixel_map):
+            for x, pixel in enumerate(row):
+                if pixel == "d":
+                    x0 = x_start + x * self.size
+                    y0 = y_start + y * self.size
+                    draw_tool.rectangle([x0, y0, x0 + self.size, y0 + self.size], fill = (0, 0, 0))
--- py_code/main.py
+++ py_code/main.py
@@ -28,6 +28,14 @@ from Score import ScoreBPlus
 from Score import ScoreB
 from Score import ScoreCPlus
 from Score import ScoreC
+from Term import Term1_1
+from Term import Term1_2
+from Term import Term2_1
+from Term import Term2_2
+from Term import Term3_1
+from Term import Term3_2
+from Term import Term4_1
+from Term import Term4_2
 
 def main():
     joystick = Joystick()
@@ -47,39 +55,42 @@ def main():
         Dot1(60, 41), Dot1(67, 14), Dot1(70, 80), Dot1(93, 205),
         Dot1(96, 101), Dot1(159, 183), Dot1(163, 160), Dot1(171, 36),
         Dot1(180, 84), Dot1(184, 154), Dot1(218, 118), Dot1(223, 103)
-    ]
+        ]
     dots2 = [
         Dot2(1, 169), Dot2(6, 76), Dot2(29, 12), Dot2(35, 220),
         Dot2(60, 92), Dot2(61, 57), Dot2(62, 111), Dot2(97, 137),
         Dot2(120, 160), Dot2(140, 129), Dot2(149, 122), Dot2(150, 18),
         Dot2(171, 165), Dot2(181, 40), Dot2(184, 60), Dot2(185, 199),
         Dot2(195, 45), Dot2(200, 210), Dot2(201, 15), Dot2(227, 119)
-    ]
+        ]
     flowers = [
         Flower(10, 100), Flower(30, 180), Flower(40, 40), Flower(40, 80),
         Flower(70, 110), Flower(150, 30), Flower(160, 190), Flower(180, 160),
         Flower(200, 70), Flower(210, 210), Flower(220, 140), Flower(220, 180)
-    ]   
+        ]   
     grass = [
         Grass(70, 30), Grass(110, 70), Grass(50, 180), Grass(100, 200),
         Grass(150, 120), Grass(25, 120), Grass(190, 10)
-    ]
+        ]
     assignments = [
         Assignment(0, 0), Assignment(30, 0), Assignment(90, 0), Assignment(150, 0), Assignment(210, 0), 
         Assignment(240, 0), Assignment(240, 30), Assignment(240, 90), Assignment(240, 150), Assignment(240, 210),
         Assignment(240, 240), Assignment(210, 240), Assignment(150, 240), Assignment(90, 240), Assignment(30, 240), 
         Assignment(0, 240), Assignment(0, 210), Assignment(0, 150), Assignment(0, 90), Assignment(0, 30), 
-    ]
+        ]
     assignmentArrows = [
         AssignmentArrow(), AssignmentArrow(), AssignmentArrow(), AssignmentArrow(), AssignmentArrow(), 
         AssignmentArrow(), AssignmentArrow(), AssignmentArrow(), AssignmentArrow(), AssignmentArrow(), 
         AssignmentArrow(), AssignmentArrow(), AssignmentArrow(), AssignmentArrow(), AssignmentArrow(), 
         AssignmentArrow(), AssignmentArrow(), AssignmentArrow(), AssignmentArrow(), AssignmentArrow(),   
         AssignmentArrow(), AssignmentArrow(), AssignmentArrow(), AssignmentArrow(), AssignmentArrow(),
-    ]
+        ]
     score = [
         ScoreAPlus(), ScoreA(), ScoreBPlus(), ScoreB(), ScoreCPlus(), ScoreC(), 
-    ]
+        ]
+    terms = [
+        Term1_1(), Term1_2(), Term2_1(), Term2_2(), Term3_1(), Term3_2(), Term4_1(), Term4_2(),
+        ]
 
     while True:
         if not joystick.button_A.value: # A pressed
@@ -105,7 +116,7 @@ def main():
     last_time = time.time()
     exam_time = time.time()
     portal_time = time.time()
-    stage_flag = [False, False, False, False, False, False, False, False,]
+    stage_flag = [False, False, False, False, False, False, False, False, False]
     i = 0
     clear = True
     while True:
@@ -130,6 +141,8 @@ def main():
         
         # 다음 단계 진입 시
         if stage_flag[my_character.term - 1] == False:
+            if stage_flag[7] == True:
+                break
             my_draw.rectangle((0, 0, joystick.width, joystick.height), fill = (155, 219, 71))
             for d1 in dots1:
                 d1.draw(my_draw)
@@ -139,6 +152,9 @@ def main():
                 f.draw(my_draw)
             for g in grass:
                 g.draw(my_draw)
+
+            terms[my_character.term - 1].setSize(100, 10, 3)
+            terms[my_character.term - 1].draw(my_draw)
 
             joystick.disp.image(my_image)
 
@@ -154,8 +170,6 @@ def main():
             i = 0
 
             stage_flag[my_character.term - 1] = True
-            if stage_flag[7] == True:
-                break
 
 
 
@@ -210,6 +224,8 @@ def main():
 
         arrows = [assignmentArrow for assignmentArrow in arrows if 0 <= assignmentArrow.position[0] <= 240 and 0 <= assignmentArrow.position[1] <= 240 and assignmentArrow.state == 'alive']
 
+        
+
         if portal.state == 'alive':
             portal.collision_check(my_character)
 
@@ -224,6 +240,8 @@ def main():
         for g in grass:
             g.draw(my_draw)
 
+        if portal.state != 'die':
+            terms[my_character.term - 1].draw(my_draw)
 
         if my_character.grade < 6:    
             my_character.draw(my_draw)
